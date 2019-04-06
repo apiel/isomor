d57d35a384b78a529de6e66a50ce02c6bc5237c3
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-06 09:35:40 +0200

    no need macro folder

--- packages/isomor-macro/dist/macro.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { createMacro, MacroError } = require('babel-plugin-macros');
-exports.default = createMacro(myMacro);
-function myMacro({ references, state, babel }) {
-    console.log('references', references.default[0]);
-    references.default[0].replaceWith(babel.types.stringLiteral('hi there'));
-}
-//# sourceMappingURL=macro.js.map

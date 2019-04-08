f2cb1176feecc76de689680fec3974e7346fb165
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-08 22:51:45 +0200

    isomor-react-app

--- packages/isomor-react-app/dist/index.js
+++ packages/isomor-react-app/dist/index.js
@@ -2,18 +2,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fancy_log_1 = require("fancy-log");
+const fs_extra_1 = require("fs-extra");
+const path_1 = require("path");
 const child_process_1 = require("child_process");
+const minimist = require("minimist");
 function start(options) {
-    fancy_log_1.info('Setup create-react-app with isomor', process.cwd());
-    const npx = child_process_1.spawn('npx', ['create-react-app', '--help']);
+    const { srcFolder, distAppFolder } = options;
+    fancy_log_1.info('Setup create-react-app with isomor');
+    const { _: [projectDirectory] } = minimist(process.argv.slice(2));
+    const npx = child_process_1.spawn('npx', ['create-react-app', projectDirectory, '--typescript']);
     npx.stdout.on('data', data => {
-        fancy_log_1.info(data.toString());
+        process.stdout.write(data.toString());
     });
     npx.stderr.on('data', data => {
-        fancy_log_1.error(data.toString());
+        process.stdout.write(data.toString());
     });
     npx.on('close', code => {
         fancy_log_1.info(`child process exited with code ${code}`);
+        fs_extra_1.copySync(path_1.join(__dirname, '..', 'tsconfig.server.json'), path_1.join(projectDirectory, 'tsconfig.server.json'));
+        fs_extra_1.copySync(path_1.join(projectDirectory, distAppFolder), path_1.join(projectDirectory, srcFolder));
+        const pkg = fs_extra_1.readJSONSync(path_1.join(projectDirectory, 'package.json'));
+        pkg.proxy = 'http://127.0.0.1:3005';
+        const pkgExample = fs_extra_1.readJSONSync(path_1.join(__dirname, '..', '..', 'example', 'package.json'));
+        pkg.scripts = pkgExample.scripts;
+        fs_extra_1.writeJSONSync(path_1.join(projectDirectory, 'package.json'), pkg);
+        const output = child_process_1.execSync(`cd ${projectDirectory} && yarn add isomor isomor-react`);
+        fancy_log_1.info(output.toString());
     });
 }
 start({
--- packages/isomor-react-app/lib/index.ts
+++ packages/isomor-react-app/lib/index.ts
@@ -4,6 +4,7 @@ import { info, error } from 'fancy-log'; // fancy log not so fancy, i want color
 import { copySync, readJSONSync, writeJSONSync } from 'fs-extra';
 import { join } from 'path';
 import { spawn, execSync } from 'child_process';
+import chalk from 'chalk';
 import * as minimist from 'minimist';
 
 interface Options {
@@ -14,31 +15,38 @@ interface Options {
 function start(options: Options) {
     const { srcFolder, distAppFolder } = options;
     info('Setup create-react-app with isomor');
+    info('Install create-react-app');
     const { _: [projectDirectory] } = minimist(process.argv.slice(2));
     const npx = spawn('npx', ['create-react-app', projectDirectory, '--typescript']);
     npx.stdout.on('data', data => {
-        process.stdout.write(data.toString());
+        process.stdout.write(chalk.green(data.toString()));
     });
     npx.stderr.on('data', data => {
-        process.stdout.write(data.toString()); // need to use chalk
+        process.stdout.write(chalk.red(data.toString())); // need to use chalk
     });
-    npx.on('close', code => {
-        info(`child process exited with code ${code}`);
+    npx.on('close', () => {
+        info('Copy tsconfig.server.json');
         copySync(
             join(__dirname, '..', 'tsconfig.server.json'),
             join(projectDirectory, 'tsconfig.server.json'),
         );
+        info(`Copy ${distAppFolder} to ${srcFolder}`);
         copySync(
             join(projectDirectory, distAppFolder),
             join(projectDirectory, srcFolder),
         );
+        info('Edit package.json');
         const pkg = readJSONSync(join(projectDirectory, 'package.json'));
         pkg.proxy = 'http://127.0.0.1:3005';
         const pkgExample = readJSONSync(join(__dirname, '..', '..', 'example', 'package.json'));
         pkg.scripts = pkgExample.scripts; // should make diff
         writeJSONSync(join(projectDirectory, 'package.json'), pkg);
+
+        info('Install isomor-react');
         const output = execSync(`cd ${projectDirectory} && yarn add isomor isomor-react`);
         info(output.toString());
+
+        // need to setup isomor-react in <App />
     });
 }
 

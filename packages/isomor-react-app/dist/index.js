b3b7795474ac5d3a657072d03deaf59aaa7f87b6
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-08 22:34:42 +0200

    isomor-react-app

--- packages/isomor-react-app/dist/index.js
+++ packages/isomor-react-app/dist/index.js
@@ -4,9 +4,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fancy_log_1 = require("fancy-log");
 const child_process_1 = require("child_process");
 function start(options) {
-    fancy_log_1.info('Setup create-react-app with isomor');
-    const output = child_process_1.execSync('npx create-react-app --help');
-    console.log('output:', output);
+    fancy_log_1.info('Setup create-react-app with isomor', process.cwd());
+    const npx = child_process_1.spawn('npx', ['create-react-app', '--help']);
+    npx.stdout.on('data', data => {
+        fancy_log_1.info(data.toString());
+    });
+    npx.stderr.on('data', data => {
+        fancy_log_1.error(data.toString());
+    });
+    npx.on('close', code => {
+        fancy_log_1.info(`child process exited with code ${code}`);
+    });
 }
 start({
     srcFolder: process.env.SRC_FOLDER || './src-isomor',
--- packages/isomor-react-app/lib/index.ts
+++ packages/isomor-react-app/lib/index.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-import { info } from 'fancy-log'; // fancy log not so fancy, i want colors :D
-import { copySync } from 'fs-extra';
+import { info, error } from 'fancy-log'; // fancy log not so fancy, i want colors :D
+import { copySync, readJSONSync, writeJSONSync } from 'fs-extra';
 import { join } from 'path';
-import { execSync } from 'child_process';
+import { spawn, execSync } from 'child_process';
+import * as minimist from 'minimist';
 
 interface Options {
     srcFolder: string;
@@ -11,9 +12,34 @@ interface Options {
 }
 
 function start(options: Options) {
+    const { srcFolder, distAppFolder } = options;
     info('Setup create-react-app with isomor');
-    const output = execSync('npx create-react-app --help');
-    console.log('output:', output.toString());
+    const { _: [projectDirectory] } = minimist(process.argv.slice(2));
+    const npx = spawn('npx', ['create-react-app', projectDirectory, '--typescript']);
+    npx.stdout.on('data', data => {
+        process.stdout.write(data.toString());
+    });
+    npx.stderr.on('data', data => {
+        process.stdout.write(data.toString()); // need to use chalk
+    });
+    npx.on('close', code => {
+        info(`child process exited with code ${code}`);
+        copySync(
+            join(__dirname, '..', 'tsconfig.server.json'),
+            join(projectDirectory, 'tsconfig.server.json'),
+        );
+        copySync(
+            join(projectDirectory, distAppFolder),
+            join(projectDirectory, srcFolder),
+        );
+        const pkg = readJSONSync(join(projectDirectory, 'package.json'));
+        pkg.proxy = 'http://127.0.0.1:3005';
+        const pkgExample = readJSONSync(join(__dirname, '..', '..', 'example', 'package.json'));
+        pkg.scripts = pkgExample.scripts; // should make diff
+        writeJSONSync(join(projectDirectory, 'package.json'), pkg);
+        const output = execSync(`cd ${projectDirectory} && yarn add isomor isomor-react`);
+        info(output.toString());
+    });
 }
 
 start({

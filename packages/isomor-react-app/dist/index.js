c9a4b2d60fb12e5eef1b9ccb4ddd881cdbabdc60
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-10 14:49:46 +0200

    add docker to be able to test in container

--- packages/isomor-react-app/dist/index.js
+++ packages/isomor-react-app/dist/index.js
@@ -17,36 +17,42 @@ const chalk_1 = require("chalk");
 const minimist = require("minimist");
 function start(options) {
     return __awaiter(this, void 0, void 0, function* () {
-        const { srcFolder, distAppFolder } = options;
-        fancy_log_1.info('Setup create-react-app with isomor');
-        fancy_log_1.info('Install create-react-app');
-        let { _: [projectDirectory] } = minimist(process.argv.slice(2));
-        projectDirectory = path_1.join(process.cwd(), projectDirectory);
-        if (!projectDirectory) {
-            fancy_log_1.warn(`${chalk_1.default.yellow('Please provide the project directory')} e.g: npx isomor-react-app my-app`);
-            return;
+        try {
+            const { srcFolder, distAppFolder } = options;
+            fancy_log_1.info('Setup create-react-app with isomor');
+            let { _: [projectDirectory] } = minimist(process.argv.slice(2));
+            projectDirectory = path_1.join(process.cwd(), projectDirectory);
+            fancy_log_1.info('Install create-react-app in', projectDirectory);
+            if (!projectDirectory) {
+                fancy_log_1.warn(`${chalk_1.default.yellow('Please provide the project directory')} e.g: npx isomor-react-app my-app`);
+                return;
+            }
+            yield shell('npx', ['create-react-app', projectDirectory, '--typescript']);
+            fancy_log_1.info('Copy tsconfig.server.json');
+            fs_extra_1.copySync(path_1.join(__dirname, '..', 'tsconfig.server.json'), path_1.join(projectDirectory, 'tsconfig.server.json'));
+            fancy_log_1.info(`Copy ${distAppFolder} to ${srcFolder}`);
+            fs_extra_1.copySync(path_1.join(projectDirectory, distAppFolder), path_1.join(projectDirectory, srcFolder));
+            fancy_log_1.info('Edit package.json');
+            const pkg = fs_extra_1.readJSONSync(path_1.join(projectDirectory, 'package.json'));
+            pkg.proxy = 'http://127.0.0.1:3005';
+            const pkgExample = fs_extra_1.readJSONSync(path_1.join(__dirname, '..', 'package-copy.json'));
+            pkg.scripts = pkgExample.scripts;
+            fs_extra_1.writeJSONSync(path_1.join(projectDirectory, 'package.json'), pkg);
+            fancy_log_1.info('Install isomor-react');
+            fs_extra_1.writeFileSync('cmd', `cd ${projectDirectory} && yarn add isomor isomor-react && yarn add npm-run-all nodemon --dev`);
+            yield shell('bash', ['cmd']);
+            fs_extra_1.unlinkSync('cmd');
+            fancy_log_1.info('Setup isomor-react in <App />');
+            const index = `import { IsomorProvider } from 'isomor-react';\n`
+                + fs_extra_1.readFileSync(path_1.join(projectDirectory, srcFolder, 'index.tsx')).toString();
+            const newIndex = index.replace(/\<App \/\>/g, '(<IsomorProvider><App /></IsomorProvider>)');
+            fs_extra_1.writeFileSync(path_1.join(projectDirectory, srcFolder, 'index.tsx'), newIndex);
+            fancy_log_1.info(`Ready to code :-) ${chalk_1.default.bold(chalk_1.default.red('Important: ') + chalk_1.default.blue(`edit you code in ${srcFolder}`))} instead of ${distAppFolder}`);
         }
-        yield shell('npx', ['create-react-app', projectDirectory, '--typescript']);
-        fancy_log_1.info('Copy tsconfig.server.json');
-        fs_extra_1.copySync(path_1.join(__dirname, '..', 'tsconfig.server.json'), path_1.join(projectDirectory, 'tsconfig.server.json'));
-        fancy_log_1.info(`Copy ${distAppFolder} to ${srcFolder}`);
-        fs_extra_1.copySync(path_1.join(projectDirectory, distAppFolder), path_1.join(projectDirectory, srcFolder));
-        fancy_log_1.info('Edit package.json');
-        const pkg = fs_extra_1.readJSONSync(path_1.join(projectDirectory, 'package.json'));
-        pkg.proxy = 'http://127.0.0.1:3005';
-        const pkgExample = fs_extra_1.readJSONSync(path_1.join(__dirname, '..', '..', 'example', 'package.json'));
-        pkg.scripts = pkgExample.scripts;
-        fs_extra_1.writeJSONSync(path_1.join(projectDirectory, 'package.json'), pkg);
-        fancy_log_1.info('Install isomor-react');
-        fs_extra_1.writeFileSync('cmd', `cd ${projectDirectory} && yarn add isomor isomor-react && yarn add npm-run-all nodemon --dev`);
-        yield shell('bash', ['cmd']);
-        fs_extra_1.unlinkSync('cmd');
-        fancy_log_1.info('Setup isomor-react in <App />');
-        const index = `import { IsomorProvider } from 'isomor-react';\n`
-            + fs_extra_1.readFileSync(path_1.join(projectDirectory, srcFolder, 'index.tsx')).toString();
-        const newIndex = index.replace(/\<App \/\>/g, '(<IsomorProvider><App /></IsomorProvider>)');
-        fs_extra_1.writeFileSync(path_1.join(projectDirectory, srcFolder, 'index.tsx'), newIndex);
-        fancy_log_1.info(`Ready to code :-) ${chalk_1.default.bold(chalk_1.default.red('Important: ') + chalk_1.default.blue(`edit you code in ${srcFolder}`))} instead of ${distAppFolder}`);
+        catch (err) {
+            fancy_log_1.error(err);
+            process.exit(1);
+        }
     });
 }
 function shell(command, args) {

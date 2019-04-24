165ca3bbc95618cd4d5704480c6b6507164300cc
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-24 18:44:28 +0200

    published

---
+++ packages/isomor-transpiler/dist/transform.integration.test.js
@@ -0,0 +1,63 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const generator_1 = require("@babel/generator");
+const typescript_estree_1 = require("@typescript-eslint/typescript-estree");
+const transform_1 = require("./transform");
+const codeSource = `
+import { readdir } from 'fs-extra';
+import { CpuInfo } from 'os';
+
+export type MyType = string;
+export interface MyInterface {
+    foo: CpuInfo;
+    bar: {
+        child: CpuInfo;
+    };
+}
+
+export function getTime1(): Promise<string[]> {
+    return readdir('./');
+}
+
+export async function getTime2(input: { foo: string }): Promise<string[]> {
+    return readdir('./');
+}
+
+export const getTime3 = async (hello: string) => {
+    return await readdir('./');
+};
+
+function shouldNotBeTranspiled() {
+    console.log('hello');
+}
+`;
+const codeTranspiled = `import { remote } from "isomor";
+export type MyType = any;
+export interface MyInterface {
+  foo: any;
+  bar: {
+    child: any;
+  };
+}
+export function getTime1(...args: any) {
+  return remote("path/to/file", "getTime1", args);
+}
+export function getTime2(...args: any) {
+  return remote("path/to/file", "getTime2", args);
+}
+export const getTime3 = (...args: any) => {
+  return remote("path/to/file", "getTime3", args);
+};`;
+describe('transform', () => {
+    const path = 'path/to/file';
+    const withTypes = true;
+    describe('transform/transform()', () => {
+        it('should generate inport for isomor', () => {
+            const program = typescript_estree_1.parse(codeSource);
+            program.body = transform_1.default(program.body, path, withTypes);
+            const { code } = generator_1.default(program);
+            expect(code).toEqual(codeTranspiled);
+        });
+    });
+});
+//# sourceMappingURL=transform.integration.test.js.map

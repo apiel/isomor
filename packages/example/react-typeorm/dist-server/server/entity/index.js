87edecdf32b7ff0642a16f79e82610c767314871
Alexandre Piel <alexandre.piel@gmail.com>
2019-05-05 14:10:20 +0200

    tuncate

--- packages/example/react-typeorm/dist-server/server/entity/index.js
+++ packages/example/react-typeorm/dist-server/server/entity/index.js
@@ -9,18 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
-const User_1 = require("./User");
-var User_2 = require("./User");
-exports.User = User_2.User;
+var User_1 = require("./User");
+exports.User = User_1.User;
 typeorm_1.createConnection().then((conn) => __awaiter(this, void 0, void 0, function* () {
-    console.log('Cleanup user table.');
-    yield conn.manager.clear(User_1.User);
-    console.log('Inserting a new user into the database...');
-    const user = new User_1.User();
-    user.firstName = 'Timber';
-    user.lastName = 'Martin';
-    user.age = 25;
-    yield conn.manager.save(user);
-    console.log(`Saved a new user with id: ${user.id}`);
 })).catch(error => console.error(error));
 exports.db = () => typeorm_1.getConnection();
--- packages/example/react-typeorm/src-isomor/server/entity/index.ts
+++ packages/example/react-typeorm/src-isomor/server/entity/index.ts
@@ -3,16 +3,22 @@ import { User } from './User';
 export { User } from './User';
 
 createConnection().then(async conn => {
-    console.log('Cleanup user table.');
-    await conn.manager.clear(User);
+    // console.log('Cleanup user table.');
+    // await conn.manager.clear(User);
 
-    console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.firstName = 'Timber';
-    user.lastName = 'Martin';
-    user.age = 25;
-    await conn.manager.save(user);
-    console.log(`Saved a new user with id: ${user.id}`);
+    // console.log('Inserting a new user into the database...');
+    // const user = new User();
+    // user.firstName = 'Alexandre';
+    // user.lastName = 'Piel';
+    // user.age = 33;
+    // await conn.manager.save(user);
+
+    // const user2 = new User();
+    // user2.firstName = 'Edith';
+    // user2.lastName = 'Piaf';
+    // user2.age = 59;
+    // await conn.manager.save(user2);
+    // console.log(`Saved a new user with id: ${user2.id}`);
 }).catch(error => console.error(error));
 
 export const db = () => getConnection();

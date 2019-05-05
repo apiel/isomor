202d56c1e8a99cac7d9bbc8e4df6033256f80c51
Alexandre Piel <alexandre.piel@gmail.com>
2019-05-05 10:59:52 +0200

    update react app

--- packages/isomor-react-app/example/server/uptime.ts
+++ packages/isomor-react-app/example/server/uptime.ts
@@ -1,4 +1,4 @@
 
-export async function getServerUptime(): Promise<string> {
-    return process.uptime().toString();
+export async function getServerUptime(): Promise<number> {
+    return process.uptime();
 }
--- packages/isomor-react-app/example/uptime.tsx
+++ packages/isomor-react-app/example/uptime.tsx
@@ -3,7 +3,7 @@ import { getServerUptime } from './server/uptime';
 // import { useAsyncCacheEffect } from 'react-async-cache';
 
 export const Uptime = () => {
-    const [uptime, setUptime] = React.useState<string>();
+    const [uptime, setUptime] = React.useState<number>();
     const call = async () => setUptime(await getServerUptime());
     React.useEffect(() => { call(); }, []);
     // or instead of the last 3 lines, use react-async-cache

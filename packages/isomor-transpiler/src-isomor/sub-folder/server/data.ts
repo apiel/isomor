b54b48b0bdc36bf3b9f36b99be41874e24aecfeb
Alexandre Piel <alexandre.piel@gmail.com>
2019-05-09 15:41:17 +0200

    cleanup

--- packages/isomor-transpiler/src-isomor/sub-folder/server/data.ts
+++
@@ -1,6 +0,0 @@
-import { readdir } from 'fs-extra';
-
-export async function getList(): Promise<string[]> {
-    const files = await readdir('./');
-    return files.map(file => `${file}-${Math.random()}`);
-}
--- packages/isomor-transpiler/src/sub-folder/server/data.ts
+++
@@ -1,5 +0,0 @@
-import { remote } from "isomor";
-import { readdir } from "fs-extra";
-export function getList(...args: any) {
-  return remote("sub-folder-server-data", "getList", args);
-}

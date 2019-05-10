31e4e0f7fce66474658bb589b492827578803e0d
Alexandre Piel <alexandre.piel@gmail.com>
2019-05-10 18:15:40 +0200

    validation

--- packages/example/react-validator/src-isomor/App.tsx
+++ packages/example/react-validator/src-isomor/App.tsx
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { setUser, Input } from './server/data';
+import { validate } from 'class-validator';
 
 const App = () => {
+  const [clientErrors, setClientErrors] = React.useState<string>('');
   const [result, setResult] = React.useState<string>('');
   const [input, setInput] = React.useState<Input>(new Input());
   const send = async () => {
     setResult(await setUser(input));
   }
-  const onChangeInput = (key: 'name' | 'email') => ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+  const onChangeInput = (key: 'name' | 'email') => async ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
     input[key] = value;
+    const errors = await validate(input);
+    setClientErrors(JSON.stringify(errors, null, 4));
     setInput(input);
   }
 
@@ -22,11 +26,15 @@ const App = () => {
           <div className="App-content">
             <p>Name: <input onChange={onChangeInput('name')} /></p>
             <p>Email: <input onChange={onChangeInput('email')} /></p>
-            <button onClick={send}>Send</button>
-            {result && <p>
-              <b>Server result:</b>
+            <button onClick={send}>Server validation</button>
+            {result && <>
+              <p><b>Server result:</b></p>
               <pre>{ result }</pre>
-            </p>}
+            </>}
+            {clientErrors && <>
+              <p><b>Client validation:</b></p>
+              <pre>{ clientErrors }</pre>
+            </>}
           </div>
       </div>
   );

39e419728bb5d337517faa99e319346399ffd90b
Alexandre Piel <alexandre.piel@gmail.com>
2019-05-13 19:16:03 +0200

    angular wip

---
+++ packages/example/ng-nest/src-isomor/server/api.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common'; // > import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ApiService {
+  constructor() {}
+}
---
+++ packages/example/ng-nest/src/server/api.service.ts
@@ -0,0 +1,11 @@
+import { remote } from "isomor";
+import { Injectable } from '@angular/core';
+
+// > import { Injectable } from '@angular/core';
+@Injectable()
+class ApiService__deco_export__ {}
+
+export class ApiService extends ApiService__deco_export__ {
+  constructor(...args: any) {}
+
+}

844797414f5696de0f8f2e62640b7b623b0685a2
Alex acer lubuntu <alexandre.piel@gmail.com>
2019-05-22 22:02:08 +0200

    remove comment

--- packages/example/react-typeorm/src-isomor/server/data.ts
+++ packages/example/react-typeorm/src-isomor/server/data.ts
@@ -2,13 +2,6 @@ import { User } from './entity/User';
 import { getConnection } from 'typeorm';
 
 export { User } from './entity/User';
-// or could use interface and class User implements IUser
-// export interface IUser {
-//     id: number;
-//     firstName: string;
-//     lastName: string;
-//     age: number;
-// }
 
 export async function getList(): Promise<User[]> {
     return getConnection().manager.find(User);

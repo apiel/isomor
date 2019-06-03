497afc045dd8297299b3126e3981c4da495a67f8
Alexandre Piel <alexandre.piel@gmail.com>
2019-06-03 22:28:52 +0200

    refact

--- packages/isomor-json-schema-generator/src/NodeParser/FuncTypeNodeParser.ts
+++ packages/isomor-json-schema-generator/src/NodeParser/FuncTypeNodeParser.ts
@@ -2,11 +2,8 @@ import * as ts from "typescript";
 import { Context, NodeParser } from "../NodeParser";
 import { SubNodeParser } from "../SubNodeParser";
 import { BaseType } from "../Type/BaseType";
-import { ObjectProperty, ObjectType } from "../Type/ObjectType";
-import { getKey } from "../Utils/nodeKey";
-import { TupleType } from "../Type/TupleType";
-import { referenceHidden } from "../Utils/isHidden";
 import { OptionalType } from "../Type/OptionalType";
+import { TupleType } from "../Type/TupleType";
 
 export class FuncTypeNodeParser implements SubNodeParser {
     public constructor(
@@ -25,33 +22,11 @@ export class FuncTypeNodeParser implements SubNodeParser {
         const types = node.parameters
                         .map((item) => {
                             // console.log("item", item);
-                            const type = this.childNodeParser.createType(item.type as any, context);
+                            // what if item.type does not exist?
+                            const type = this.childNodeParser.createType(item.type!, context);
                             return item.questionToken ? new OptionalType(type) : type;
                         });
         // console.log("types", types);
         return new TupleType(types);
-    }
-
-    // public createType(node: ts.FunctionDeclaration, context: Context): BaseType {
-    //     return new ObjectType(
-    //         this.getTypeId(node, context),
-    //         [],
-    //         this.getParameters(node, context),
-    //         false,
-    //     );
-    // }
-
-    private getParameters(node: ts.FunctionDeclaration | ts.ArrowFunction, context: Context): ObjectProperty[] {
-        return node.parameters.map(paramNode => {
-            return new ObjectProperty(
-                paramNode.name.getText(),
-                this.childNodeParser.createType(paramNode.type!, context),
-                !paramNode.questionToken,
-            );
-        });
-    }
-
-    private getTypeId(node: ts.Node, context: Context): string {
-        return `function-${getKey(node, context)}`;
     }
 }

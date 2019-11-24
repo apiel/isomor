7edba94bc8f314ebd5f22ff006e8c0869ac9bbc0
Alexandre Piel <alexandre.piel@gmail.com>
2019-11-24 17:17:16 +0100

    try to fix upgrade

--- packages/isomor-server/dist/lib/apidoc.js
+++ packages/isomor-server/dist/lib/apidoc.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -18,7 +19,7 @@ function getApiDoc(endpoints) {
             if (validationSchema && validationSchema.schema) {
                 $ref = validationSchema.schema.$ref;
                 $schema = validationSchema.schema.$schema;
-                definitions = Object.assign({}, definitions, validationSchema.schema.definitions);
+                definitions = Object.assign(Object.assign({}, definitions), validationSchema.schema.definitions);
             }
             paths[path] = {
                 post: {

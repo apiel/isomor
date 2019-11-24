5ad470b233a0d3d559cff313afbc29f90f59151f
Alexandre Piel <alexandre.piel@gmail.com>
2019-11-24 16:08:46 +0100

    fix vue example

--- packages/example/vue/src-isomor/App.vue
+++ packages/example/vue/src-isomor/App.vue
@@ -7,27 +7,18 @@
       </li>
     </ul>
     <button @click="load()">load</button>
-    <TwoColumn />
-    <Stats />
     <ErrorExample />
-    <ErrorExample2 />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import TwoColumn from "./components/TwoColumn.vue";
-import Stats from "./components/status/Stats.vue";
 import ErrorExample from "./components/error/ErrorExample.vue";
-import ErrorExample2 from "./components/error/ErrorExample2.vue";
 import { getList } from "./server/data";
 
 @Component({
   components: {
-    TwoColumn,
-    Stats,
     ErrorExample,
-    ErrorExample2,
   }
 })
 export default class App extends Vue {
--- packages/example/vue/src-isomor/components/status/server/getTime.ts
+++
@@ -1,8 +0,0 @@
-
-export interface ServerTime {
-    time: string;
-}
-
-export async function getTime(): Promise<ServerTime> {
-    return { time: (new Date()).toLocaleString() };
-}

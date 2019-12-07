33d6cc4725c2bc5062fab9fdbf7cc20361221ee7
Alexandre Piel <alexandre.piel@gmail.com>
2019-12-07 22:46:39 +0100

    a bit of refactoring

--- packages/example/react/src-isomor/color/color.tsx
+++ packages/example/react/src-isomor/color/color.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { getColor, setColor } from './server/color';
 
+const onClickColor = (
+    setStateColor: React.Dispatch<React.SetStateAction<string | undefined>>,
+    newColor: string,
+) => async () => {
+    await setColor(newColor);
+    setStateColor(newColor);
+}
+
 export const Color = () => {
     const [color, setStateColor] = React.useState<string>();
 
     React.useEffect(() => { getColor().then(setStateColor); }, []);
-    const onClickColor = (newColor: string) => async () => {
-        console.log('click color', newColor);
-        await setColor(newColor);
-        setStateColor(newColor);
-    }
+
     return (
         <div style={{ color }}>
             <b>{ color }</b> Choose a color &nbsp;
-            <button onClick={onClickColor('blue')}>blue</button>
-            <button onClick={onClickColor('red')}>red</button>
-            <button onClick={onClickColor('green')}>green</button>
-            <button onClick={onClickColor('pink')}>pink</button>
+            <button onClick={onClickColor(setStateColor, 'blue')}>blue</button>
+            <button onClick={onClickColor(setStateColor, 'red')}>red</button>
+            <button onClick={onClickColor(setStateColor, 'green')}>green</button>
+            <button onClick={onClickColor(setStateColor, 'pink')}>pink</button>
         </div>
     );
 }

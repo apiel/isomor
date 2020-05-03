a38ca1c5582b577c1a20025c6209b9d0f5471f72
Alexandre Piel <alexandre.piel@gmail.com>
2020-05-03 22:13:47 +0200

    clean up

--- packages/ci-test/react-app/jest-puppeteer.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    server: {
-        command: `yarn serv:prod`,
-        port: 3005,
-        launchTimeout: 60000,
-        debug: true,
-    },
-}
--- packages/ci-test/react-app/src-isomor/server/data.ts
+++
@@ -1,7 +0,0 @@
-import { outputFile, readFile } from 'fs-extra';
-
-export const getHello = async () => {
-    const file = './data/hello.txt';
-    await outputFile(file, 'hello world');
-    return (await readFile(file)).toString();
-}

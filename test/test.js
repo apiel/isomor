812fd9942a4b80cb2fe93db333496bc554ec3a4f
Alexandre Piel <alexandre.piel@gmail.com>
2020-05-04 22:51:43 +0200

    ci

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const { exec } = require('child_process');
+
+process = exec('npx.run ismor-server', (err, output) => {
+    if (err) {
+        process.stderr.write(err);
+    }
+    process.stdout.write(output);
+});
+
+const getValue = require('api/getValue');
+getValue().then(val => {
+    if (val === 123) {
+        process.stdout.write('Success\n');
+        process.exit(0);
+    } else {
+        process.stderr.write('Failed\n');
+        process.exit(1);
+    }
+});

8e16aa0bf309a790a23326be16da35a424db9776
Alexandre Piel <alexandre.piel@gmail.com>
2020-05-04 23:01:46 +0200

    ci with ubuntu

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-process = exec('npx.run ismor-server', (err, output) => {
+process = exec('npx.run isomor-server', (err, output) => {
     if (err) {
         process.stderr.write(err);
     }
